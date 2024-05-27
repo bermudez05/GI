8d9a536655cac20a4ef9bd112d6a425b3f0d67b5
Nicole Bermudez <60791862+bermudez05@users.noreply.github.com>
2024-05-26 20:38:44 -0500

    .

---
+++ JS/mapdata.js
@@ -0,0 +1,361 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "white",
+    background_transparent: "no",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#95aa61",
+    state_hover_color: "#d6e68a",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    location_description: "",
+    location_url: "",
+    location_color: "#9b0000",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    CNAH: {
+      name: "Anhui Province"
+    },
+    CNBJ: {
+      name: "Beijing Municipality",
+      color: "#8c873e"
+    },
+    CNCQ: {
+      name: "Chongqing Municipality"
+    },
+    CNFJ: {
+      name: "Fujian Province"
+    },
+    CNGD: {
+      name: "Guangdong Province"
+    },
+    CNGS: {
+      name: "Gansu province"
+    },
+    CNGX: {
+      name: "Guangxi Zhuang Autonomous Region"
+    },
+    CNGZ: {
+      name: "Guizhou Province"
+    },
+    CNHA: {
+      name: "Henan Province"
+    },
+    CNHB: {
+      name: "Hubei Province",
+      color: "#8c873e"
+    },
+    CNHE: {
+      name: "Hebei Province"
+    },
+    CNHI: {
+      name: "Hainan Province"
+    },
+    CNHK: {
+      name: "Hong Kong Special Administrative Region"
+    },
+    CNHL: {
+      name: "Heilongjiang Province"
+    },
+    CNHN: {
+      name: "Hunan Province",
+      color: "#8c873e"
+    },
+    CNJL: {
+      name: "Jilin Province"
+    },
+    CNJS: {
+      name: "Jiangsu Province",
+      color: "#8c873e"
+    },
+    CNJX: {
+      name: "Jiangxi Province"
+    },
+    CNLN: {
+      name: "Liaoning Province"
+    },
+    CNMO: {
+      name: "Macao Special Administrative Region"
+    },
+    CNNM: {
+      name: "Inner Mongolia Autonomous Region"
+    },
+    CNNX: {
+      name: "Ningxia Hui Autonomous Region"
+    },
+    CNQH: {
+      name: "Qinghai Province"
+    },
+    CNSC: {
+      name: "Sichuan Province",
+      color: "#8c873e"
+    },
+    CNSD: {
+      name: "Shandong Province"
+    },
+    CNSH: {
+      name: "Shanghai Municipality"
+    },
+    CNSN: {
+      name: "Shaanxi Province"
+    },
+    CNSX: {
+      name: "Shanxi Province"
+    },
+    CNTJ: {
+      name: "Tianjin Municipality"
+    },
+    CNTW: {
+      name: "Taiwan Province"
+    },
+    CNXJ: {
+      name: "Xinjiang Uygur Autonomous Region"
+    },
+    CNXZ: {
+      name: "Tibet Autonomous Region"
+    },
+    CNYN: {
+      name: "Yunnan Province"
+    },
+    CNZJ: {
+      name: "Zhejiang Province"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Monte Wudang",
+      lat: "32.398563",
+      lng: "111.009890",
+      description: "<img src=\"https://image.jimcdn.com/app/cms/image/transf/none/path/sda0477424a35f21c/image/ic61569c9ba3a6759/version/1607197603/image.jpg\" /> Uno de los lugares más sagrados del taoísmo, conocido por sus templos antiguos y su asociación con la alquimia taoísta y las artes marciales."
+    },
+    "1": {
+      lat: "30.966760",
+      lng: "103.516640",
+      name: "Monte Qingcheng",
+      description: "<img src=\"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/44/20/a0/entry-to-the-front-mountain.jpg?w=1200&h=-1&s=1\" /> Uno de los lugares de nacimiento del taoísmo, asociado con Lao-Tsé. Cuenta con templos antiguos y paisajes naturales impresionantes."
+    },
+    "2": {
+      lat: "39.88212934124961",
+      lng: "116.40664005378359",
+      name: "Templo del Cielo ",
+      description: "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6tO_zkvsE7UTQMH8AeVXuYI0OClTylGZ5ujKe2YPzWQ&s\" /> Construido durante la dinastía Ming, este complejo de templos es un importante sitio para los rituales taoístas de adoración al Cielo."
+    },
+    "3": {
+      lat: "27.292330204105316",
+      lng: "112.69059604994219",
+      name: "Monte Heng ",
+      description: "<img src=\"https://lh6.googleusercontent.com/proxy/Glf_SCVjgzN5stt_cJj60YRPYKpIpIsEgKpszxtpZR-9POAlCe5Nj6zVS7uQk1BDiCJ5PjoOjRh7YKzDEDBnqVvHIHepdCvyI9N0A9FgdOVrY1R61IMP4knsMjrPRNEZFw\" /> Uno de los cinco montes sagrados del taoísmo en China, conocido por su rica historia religiosa y su importancia como lugar de peregrinación."
+    },
+    "4": {
+      lat: "29.046879909932827",
+      lng: "110.48173316148794",
+      description: "<img src=\"https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/af/a4.jpg\" /> Situado en la montaña Tianmen, es conocido por su escalera celestial y sus vistas panorámicas, siendo un lugar sagrado para los practicantes taoístas.",
+      name: "Tianmenshan"
+    },
+    "5": {
+      lat: "32.2152073046644",
+      lng: "119.41608157513879",
+      name: "Jinshan Si",
+      description: "<img src=\"https://www.visitourchina.com/FileUpload/FileUpload/110420163921531.jpg\" /> Situado en la montaña Jinshan, este templo taoísta es un importante lugar de culto y peregrinación en la provincia de Jiangsu. Es conocido por su arquitectura tradicional y su impresionante entorno natural."
+    }
+  },
+  labels: {
+    CNAH: {
+      name: "Anhui Province",
+      parent_id: "CNAH"
+    },
+    CNBJ: {
+      name: "Beijing Municipality",
+      parent_id: "CNBJ"
+    },
+    CNCQ: {
+      name: "Chongqing Municipality",
+      parent_id: "CNCQ"
+    },
+    CNFJ: {
+      name: "Fujian Province",
+      parent_id: "CNFJ"
+    },
+    CNGD: {
+      name: "Guangdong Province",
+      parent_id: "CNGD"
+    },
+    CNGS: {
+      name: "Gansu province",
+      parent_id: "CNGS"
+    },
+    CNGX: {
+      name: "Guangxi Zhuang Autonomous Region",
+      parent_id: "CNGX"
+    },
+    CNGZ: {
+      name: "Guizhou Province",
+      parent_id: "CNGZ"
+    },
+    CNHA: {
+      name: "Henan Province",
+      parent_id: "CNHA"
+    },
+    CNHB: {
+      name: "Hubei Province",
+      parent_id: "CNHB"
+    },
+    CNHE: {
+      name: "Hebei Province",
+      parent_id: "CNHE"
+    },
+    CNHI: {
+      name: "Hainan Province",
+      parent_id: "CNHI"
+    },
+    CNHK: {
+      name: "Hong Kong Special Administrative Region",
+      parent_id: "CNHK"
+    },
+    CNHL: {
+      name: "Heilongjiang Province",
+      parent_id: "CNHL"
+    },
+    CNHN: {
+      name: "Hunan Province",
+      parent_id: "CNHN"
+    },
+    CNJL: {
+      name: "Jilin Province",
+      parent_id: "CNJL"
+    },
+    CNJS: {
+      name: "Jiangsu Province",
+      parent_id: "CNJS"
+    },
+    CNJX: {
+      name: "Jiangxi Province",
+      parent_id: "CNJX"
+    },
+    CNLN: {
+      name: "Liaoning Province",
+      parent_id: "CNLN"
+    },
+    CNMO: {
+      name: "Macao Special Administrative Region",
+      parent_id: "CNMO"
+    },
+    CNNM: {
+      name: "Inner Mongolia Autonomous Region",
+      parent_id: "CNNM"
+    },
+    CNNX: {
+      name: "Ningxia Hui Autonomous Region",
+      parent_id: "CNNX"
+    },
+    CNQH: {
+      name: "Qinghai Province",
+      parent_id: "CNQH"
+    },
+    CNSC: {
+      name: "Sichuan Province",
+      parent_id: "CNSC"
+    },
+    CNSD: {
+      name: "Shandong Province",
+      parent_id: "CNSD"
+    },
+    CNSH: {
+      name: "Shanghai Municipality",
+      parent_id: "CNSH"
+    },
+    CNSN: {
+      name: "Shaanxi Province",
+      parent_id: "CNSN"
+    },
+    CNSX: {
+      name: "Shanxi Province",
+      parent_id: "CNSX"
+    },
+    CNTJ: {
+      name: "Tianjin Municipality",
+      parent_id: "CNTJ"
+    },
+    CNTW: {
+      name: "Taiwan Province",
+      parent_id: "CNTW"
+    },
+    CNXJ: {
+      name: "Xinjiang Uygur Autonomous Region",
+      parent_id: "CNXJ"
+    },
+    CNXZ: {
+      name: "Tibet Autonomous Region",
+      parent_id: "CNXZ"
+    },
+    CNYN: {
+      name: "Yunnan Province",
+      parent_id: "CNYN"
+    },
+    CNZJ: {
+      name: "Zhejiang Province",
+      parent_id: "CNZJ"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
